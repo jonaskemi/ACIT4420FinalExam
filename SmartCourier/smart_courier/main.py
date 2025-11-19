ee05e8d20273d35e72583bc8e33fdbde079dcff2
jonaskemi <jonaskemi97@gmail.com>
2025-11-19 20:41:36 +0100

    added optimizer function in optimizer.py and CLI in main.

--- SmartCourier/smart_courier/main.py
+++ SmartCourier/smart_courier/main.py
@@ -1,4 +1,5 @@
 from .validation import (validate_customer_name, validate_priority, validate_latitude, validate_longitude, validate_weight)
+from .optimizer import optimizer
 import csv
 
 def validate_inputs(deliveries_file):
@@ -23,6 +24,9 @@ def validate_inputs(deliveries_file):
                 writer.writerow([customer, latitude, longitude, priority, weight_kg])
             print(f"Invalid: {line}")
             
+def run_optimizer():
+    optimizer('output/valid.csv')
+            
 def view_csv_file(file_path):
     with open(file_path, 'r') as f:
         reader = csv.reader(f)
@@ -30,44 +34,115 @@ def view_csv_file(file_path):
             print(row)
 
 def main():
-    # Welcome message and user prompt and check file existence
-    print("\nHello! Welcome to Smart Courier!")
-    deliveries_file = input("\nEnter the path to the deliveries file (press enter to use data/deliveries.csv): \n")
-    try:
-        if not deliveries_file:
-            deliveries_file = 'data/deliveries.csv'
-    except FileNotFoundError:
-        print("File not found. Please check the path and try again.")
-        return
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return
-    view_csv_file(deliveries_file)
-    
-    # Validation confirmation
-    print("\nDo you wish to validate the deliveries from file:", deliveries_file, "?")  
-    confirm = input("Type 'yes' to confirm, or anything else to exit: ")
-    if confirm.lower() != 'yes':
-        print("Exiting the program.")
-        return
-    try:
-        validate_inputs(deliveries_file=deliveries_file)
-        print("\nValidation complete. These are valid deliveries: output/valid.csv")
-        view_csv_file('output/valid.csv')
-    except Exception as e:
-        print(f"An error occurred during validation: {e}")
-        return
-    
-    # View rejected deliveries or continue to optimizer?
-    print("\nDo you wish to view the rejected deliveries?")
-    view_rejected = input("Type 'yes' to view rejected deliveries, or anything else to exit: ")
-    if view_rejected.lower() == 'yes':
-        print("\nThese are the rejected deliveries: output/rejected.csv")
-        view_csv_file('output/rejected.csv')
-    print("\nOkay! Do you wish to continue to the optimizer?")
-    continue_optimizer = input("Type 'yes' to continue to the optimizer, or anything else to exit: ")
-    if continue_optimizer.lower() == 'yes':
-        print("Continuing to the optimizer... (Functionality not yet implemented)")
-    
+    while True: 
+        print("\n==============================")
+        print(" Smart Courier Main Menu ")
+        print("==============================")
+        print("1. Start Validation and Optimization Process")
+        print("2. Reset Output Files (recommended before new runs)")
+        print("3. Exit")
+        choice = input("\nPlease enter your choice (1-3): ")
+        
+        # Start validation process
+        if choice == '1':
+            deliveries_file = input("\nEnter the path to the deliveries file (press enter to use data/deliveries.csv): \n")
+            try:
+                if not deliveries_file:
+                    deliveries_file = 'data/deliveries.csv'
+            except FileNotFoundError:
+                print("File not found. Please check the path and try again.")
+                print("==============================")
+                break
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                print("==============================")
+                break
+            view_csv_file(deliveries_file)
+            
+            # Validation confirmation
+            try:
+                print("\nDo you wish to validate the deliveries from file:", deliveries_file, "?")
+                confirm = input("Type 'yes' to confirm, or anything else to return to exit: ")
+            
+                if confirm.lower() != 'yes':
+                    print("Exiting the program.")
+                    print("==============================")
+                    break
+                elif confirm.lower() == 'yes':
+                    print("Validating.\n")
+                    validate_inputs(deliveries_file=deliveries_file)
+                    print("\nValidation complete. These are valid deliveries: output/valid.csv")
+                    print("===================================================================")
+                    view_csv_file('output/valid.csv')
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                print("==============================")
+                break
+
+            # Option to see rejected deliveries
+            try:
+                print("\nDo you wish to view the rejected deliveries?")
+                view_rejected = input("Type 'yes' to view rejected deliveries, or anything else to exit: ")
+                if view_rejected.lower() != 'yes':
+                    print("Exiting the program.")
+                    print("==============================")
+                    break
+                elif view_rejected.lower() == 'no':
+                    pass
+                elif view_rejected.lower() == 'yes':
+                    print("\nThese are the rejected deliveries: output/rejected.csv")
+                    print("===============================================================")
+                    view_csv_file('output/rejected.csv')
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                print("==============================")
+                break
+                
+            try:        
+                print("\nOkay! Do you wish to continue to the optimizer?")
+                continue_optimizer = input("Type 'yes' to continue to the optimizer, or anything else to return to main menu: ")
+        
+                if continue_optimizer.lower() != 'yes':
+                    print("Exiting the program.")
+                    print("==============================")
+                    break
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                print("==============================")
+                break
+            try:
+                run_optimizer()
+                total_distance = run_optimizer()
+                print(f"\nOptimization complete. Total distance for the route: {total_distance:.2f} km")
+                print("These are the optimized routes: output/optimized_route.csv")
+                print("====================================================================================")
+                view_csv_file('output/optimized_route.csv')
+                print("====================================================================================")
+                
+                print("\nRoute summary based on transport mode: ")
+                with open('output/optimized_route.csv', 'r') as routefile:
+                    reader = csv.reader(routefile)
+                    
+                
+            except Exception as e:
+                print(f"\nAn error occurred during optimization: {e}")
+                print("==============================")
+                break
+         
+        # Reset output files   
+        elif choice == '2':
+            open('output/valid.csv', 'w').write('customer,latitude,lognitude,priority,weight_kg\n')
+            open('output/rejected.csv', 'w').write('customer,latitude,lognitude,priority,weight_kg\n')
+            open('output/optimized_route.csv', 'w').write('from_customer,to_customer,distance_km,mode_of_transport,time,cost,emissions_kgCO2\n')
+            print("\nOutput files have been reset.")
+            print("==============================")
+        
+        # Exit program
+        elif choice == '3':
+            print("Exiting the program.")
+            break
+        else:
+            print("\nInvalid choice. Please enter a number between 1 and 3.")
+
 if __name__ == "__main__":
     main()
--- SmartCourier/smart_courier/optimizer.py
+++ SmartCourier/smart_courier/optimizer.py
@@ -1,16 +1,79 @@
-from math import radians, cos, sin, asin, sqrt
-
+from math import cos, sin, asin, sqrt
+import csv
 
 def haversine(lon1, lat1, lon2, lat2):
-    
-    # convert decimal degrees to radians 
-    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
-
     # haversine formula 
     dlon = lon2 - lon1 
     dlat = lat2 - lat1 
     a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
     c = 2 * asin(sqrt(a)) 
-    r = 6371 # Radius of earth in kilometers. Use 3956 for miles. Determines return value units.
+    r = 6371 # Radius of earth in kilometers. Determines return value units.
     return c * r
 
+def optimizer(deliveries):
+    if not deliveries:
+        return []
+    
+    with open('output/optimized_route.csv', 'w', newline='') as routefile:
+        writer = csv.writer(routefile)
+        writer.writerow(['from_customer', 'to_customer', 'distance_km'])
+    
+        start_depot = (59.94, 10.94, "Postens Terminal")            # Assuming depot is at "Postens Terminal"
+        unvisited = []                          # List of deliveries to visit 
+        current_location = start_depot          # Start at the depot
+        distance = 0.0                          # Total distance traveled    
+    
+        # Convert valid deliveries CSV to list of dicts
+        with open(deliveries, 'r') as validfile:
+            reader = csv.reader(validfile)
+            next(reader)  # Skip header
+            for row in reader:
+                customer, latitude, longitude, priority, weight_kg = row
+                unvisited.append({
+                    'customer': customer,
+                    'latitude': latitude,
+                    'longitude': longitude,
+                    'priority': priority,
+                    'weight_kg': weight_kg
+                })
+
+        while unvisited:
+            # Finding the deliveries with the highest priority first
+            high_priority = [d for d in unvisited if d['priority'].lower() == 'high']
+            medium_priority= [d for d in unvisited if d['priority'].lower() == 'medium']
+         
+            # Deliver high priority first
+            if high_priority:
+                deliveries = high_priority
+            elif medium_priority:
+                deliveries = medium_priority
+            else:
+                deliveries = unvisited
+        
+            nearest = min(
+                deliveries, # If there is high priority deliveries, only consider those then medium then low
+                key=lambda delivery: haversine(  # Uses lambda to calculate distance from current location to each delivery then pick the minimum
+                current_location[1], current_location[0],
+                float(delivery['longitude']), float(delivery['latitude'])
+                ))
+        
+            leg_distance = haversine(
+                current_location[1], current_location[0],
+                float(nearest['longitude']), float(nearest['latitude'])
+            )
+            distance += leg_distance
+            next_location = (float(nearest['latitude']), float(nearest['longitude']), nearest['customer'])    
+            writer.writerow([current_location[2], next_location[2], distance])
+        
+            # Set current to the next location and remove the visited delivery from unvisited
+            current_location = next_location
+            unvisited.remove(nearest)
+        
+        # Return to depot after all deliveries are done
+        return_leg = haversine(
+            current_location[1], current_location[0],
+            start_depot[1], start_depot[0]
+        )
+        distance += return_leg
+        writer.writerow([current_location[2], start_depot[2], distance])
+    return distance
